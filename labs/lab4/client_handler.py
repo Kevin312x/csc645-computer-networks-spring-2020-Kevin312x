f2c5707f9581087e4539a9a00163ad73d3f0ce31
Kevin Huynh <anmevee@yahoo.com>
2020-02-24 22:13:14 -0800

    lab4

--- labs/lab4/client_handler.py
+++ labs/lab4/client_handler.py
@@ -3,22 +3,23 @@
 # Date: 02/03/2020
 # Lab3: Server support for multiple clients
 # Goal: Learning Networking in Python with TCP sockets
-# Student Name:
-# Student ID:
-# Student Github Username:
+# Student Name: Kevin Huynh
+# Student ID: 916307020
+# Student Github Username: kevin312x
 # Lab Instructions: No partial credit will be given. Labs must be completed in class, and must be committed to your
 #               personal repository by 9:45 pm.
 # Running instructions: This program needs the server to run. The server creates an object of this class.
 #
 ########################################################################################################################
 import threading
+import pickle
 
 class ClientHandler(object):
     """
     The client handler class receives and process client requests
     and sends responses back to the client linked to this handler.
     """
-    def __init__(self, server_instance, clienthandler, addr):
+    def __init__(self, clienthandler, addr):
         """
         Class constructor already implemented for you.
         :param server_instance:
@@ -27,7 +28,7 @@ class ClientHandler(object):
         """
         self.clientid = addr[1] # the id of the client that owns this handler
         self.server_ip = addr[0]
-        self.server = server_instance
+        #self.server = server_instance
         self.clienthandler = clienthandler
 
     def print_lock(self):
@@ -36,7 +37,7 @@ class ClientHandler(object):
         :return: the lock created
         """
         # your code here.
-        return None # modify the return to return a the lock created
+        return threading.Lock() # modify the return to return a the lock created
 
     def process_client_data(self):
         """
@@ -48,4 +49,10 @@ class ClientHandler(object):
         TODO: release the print lock
         :return: VOID
         """
-        pass # remove this line after implemented.
+        while True:
+            self.serialized_data = pickle.loads(self.clienthandler.recv(4096))
+            lock = self.print_lock()
+            lock.acquire()
+            print(self.serialized_data)
+            lock.release()
+    
